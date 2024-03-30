76ab8d7805e0a427b9ec5086fb0cb379e314851b
Nkemjika Obi <nkemjikaobi@gmail.com>
2024-03-30 11:42:05 -0400

    refactor fetch patients to come from provider

--- lib/screens/all_patients.dart
+++ lib/screens/all_patients.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/services.dart';
-//import 'package:provider/provider.dart';
-import 'package:flutter_application_project_1/models/patient_model.dart';
+import 'package:flutter_application_project_1/providers/patient_provider.dart';
 import 'package:flutter_application_project_1/screens/patient_list_item.dart';
-import 'package:flutter_application_project_1/services/patient_service.dart';
+import 'package:provider/provider.dart';
 
 class SecondPage extends StatefulWidget {
   const SecondPage({super.key});
@@ -13,29 +11,13 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
-  List<PatientModel> patients = [];
-  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _fetchPatients();
-  }
-
-  _fetchPatients() async {
-    try {
-      var fetchedPatients = await PatientService().getAllPatients();
-      print(fetchedPatients);
-      setState(() {
-        patients = fetchedPatients;
-        isLoading = false;
-      });
-    } catch (e) {
-      setState(() {
-        isLoading = false;
-      });
-      print("An error occurred while loading patients: $e");
-    }
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      Provider.of<PatientProvider>(context, listen: false).fetchAllPatients();
+    });
   }
 
   @override
@@ -44,56 +26,64 @@ class _SecondPageState extends State<SecondPage> {
       appBar: AppBar(
         title: const Text('PATIENTS'),
       ),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
-                      hintText: 'Search...',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
+      body: Consumer<PatientProvider>(builder: (context, value, child) {
+        if (value.isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final patients = value.patients;
+        return Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Search...',
+                  border: OutlineInputBorder(),
                 ),
-                Expanded(
-                  child: RefreshIndicator(
-                    onRefresh: () => _fetchPatients(),
-                    child: ListView.builder(
-                      itemCount: patients.length,
-                      itemBuilder: (context, index) {
-                        return PatientListItem(
-                          patientData: patients[index],
-                        );
-                      },
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10, right: 10),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/addpatient');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      minimumSize: const Size(double.infinity, 40),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                    child: const Text(
-                      "Add New Patient",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-              ],
+              ),
             ),
+            Expanded(
+              child: RefreshIndicator(
+                onRefresh: () async {
+                  await Provider.of<PatientProvider>(context, listen: false)
+                      .fetchAllPatients();
+                },
+                child: ListView.builder(
+                  itemCount: patients.length,
+                  itemBuilder: (context, index) {
+                    return PatientListItem(
+                      patientData: patients[index],
+                    );
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/addpatient');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  minimumSize: const Size(double.infinity, 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                child: const Text(
+                  "Add New Patient",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+          ],
+        );
+      }),
     );
   }
 }
